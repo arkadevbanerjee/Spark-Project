a49ef4870b145d5fa9843a58fd6354d92a023f99
Arkadev Banerjee <arkadev220701@gmail.com>
2020-05-27 14:43:51 +0530

    Added new Scroll Feature Animation

--- javascript.js
+++ javascript.js
@@ -2,18 +2,7 @@
     var scroll = $(window).scrollTop();
     if(scroll < 300){
         $('nav').css('background', 'transparent');
-        /*$("nav a").hover(function(){
-  			$(this).css("color", "black");
-  			}, function(){
-  			$(this).css("color", "white";
-		});*/
     } else{
         $('nav').css('background', 'rgb(240, 173, 78)');
-        /*$('nav a').css('color', 'black');
-        $("nav a").hover(function(){
-  			$(this).css("color", "white");
-  			}, function(){
-  			$(this).css("color", "black";
-		});*/
     }
 });
---
+++ javascript2.js
@@ -0,0 +1,53 @@
+$(window).on('scroll',function(){
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > 500) {
+        $('.top-menu').removeClass('activated');
+    } else {
+        $('.top-menu').addClass('activated');
+    }
+
+    if(stop>500 && stop<1200) {
+    	$('.next-menu-1').addClass('activated');
+    }
+    else{
+    	$('.next-menu-1').removeClass('activated');
+    }
+
+    if(stop>1200 && stop<1800) {
+    	$('.next-menu-2').addClass('activated');
+    }
+    else{
+    	$('.next-menu-2').removeClass('activated');
+    }
+
+    if(stop>1800 && stop<2400) {
+    	$('.next-menu-3').addClass('activated');
+    }
+    else{
+    	$('.next-menu-3').removeClass('activated');
+    }
+    
+    if(stop>2400 && stop<3000) {
+    	$('.next-menu-4').addClass('activated');
+    }
+    else{
+    	$('.next-menu-4').removeClass('activated');
+    }
+    
+    if(stop>3000 && stop<3600) {
+    	$('.next-menu-5').addClass('activated');
+    }
+    else{
+    	$('.next-menu-5').removeClass('activated');
+    }
+    
+    if(stop>3600) {
+    	$('.next-menu-6').addClass('activated');
+    }
+    else{
+    	$('.next-menu-6').removeClass('activated');
+    }
+    
+});
