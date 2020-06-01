bed5f538ca1d17ab6f2e5c1f5487c2bc8494c5c7
Arkadev Banerjee <arkadev220701@gmail.com>
2020-06-01 11:52:06 +0530

    Updated 11:51 AM 06/01/2020

--- javascript2.js
+++ javascript2.js
@@ -2,48 +2,48 @@ $(window).on('scroll',function(){
 
     // we round here to reduce a little workload
     stop = Math.round($(window).scrollTop());
-    if (stop > 700) {
+    if (stop >= 700) {
         $('.top-menu').removeClass('activated');
     } else {
         $('.top-menu').addClass('activated');
     }
 
-    if(stop>700 && stop<1400) {
+    if(stop>700 && stop<=1400) {
     	$('.next-menu-1').addClass('activated');
     }
     else{
     	$('.next-menu-1').removeClass('activated');
     }
 
-    if(stop>1400 && stop<2200) {
+    if(stop>1400 && stop<=2200) {
     	$('.next-menu-2').addClass('activated');
     }
     else{
     	$('.next-menu-2').removeClass('activated');
     }
 
-    if(stop>2200 && stop<2800) {
+    if(stop>2200 && stop<=3000) {
     	$('.next-menu-3').addClass('activated');
     }
     else{
     	$('.next-menu-3').removeClass('activated');
     }
     
-    if(stop>2800 && stop<3600) {
+    if(stop>3000 && stop<=3800) {
     	$('.next-menu-4').addClass('activated');
     }
     else{
     	$('.next-menu-4').removeClass('activated');
     }
     
-    if(stop>3600 && stop<4900) {
+    if(stop>3800 && stop<=5100) {
     	$('.next-menu-5').addClass('activated');
     }
     else{
     	$('.next-menu-5').removeClass('activated');
     }
     
-    if(stop>4900) {
+    if(stop>5100) {
     	$('.next-menu-6').addClass('activated');
     }
     else{
