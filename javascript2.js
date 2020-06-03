44393adaaf6736e3471bb9d38f7135f155413255
Arkadev Banerjee <arkadev220701@gmail.com>
2020-06-03 09:39:16 +0530

    Updated 03/06/2020

--- javascript2.js
+++ javascript2.js
@@ -1,4 +1,4 @@
-$(window).on('scroll',function(){
+$(window).on('scroll', function() {
 
     // we round here to reduce a little workload
     stop = Math.round($(window).scrollTop());
@@ -8,46 +8,35 @@ $(window).on('scroll',function(){
         $('.top-menu').addClass('activated');
     }
 
-    if(stop>700 && stop<=1400) {
-    	$('.next-menu-1').addClass('activated');
-    }
-    else{
-    	$('.next-menu-1').removeClass('activated');
-    }
-
-    if(stop>1400 && stop<=2200) {
-    	$('.next-menu-2').addClass('activated');
-    }
-    else{
-    	$('.next-menu-2').removeClass('activated');
+    if (stop > 700 && stop <= 1400) {
+        $('.next-menu-1').addClass('activated');
+    } else {
+        $('.next-menu-1').removeClass('activated');
     }
 
-    if(stop>2200 && stop<=3000) {
-    	$('.next-menu-3').addClass('activated');
+    if (stop > 1400 && stop <= 2200) {
+        $('.next-menu-2').addClass('activated');
+    } else {
+        $('.next-menu-2').removeClass('activated');
     }
-    else{
-    	$('.next-menu-3').removeClass('activated');
+
+    if (stop > 2200 && stop <= 3000) {
+        $('.next-menu-3').addClass('activated');
+    } else {
+        $('.next-menu-3').removeClass('activated');
     }
-    
-    if(stop>3000 && stop<=3800) {
-    	$('.next-menu-4').addClass('activated');
+
+    if (stop > 3000 && stop <= 4400) {
+        $('.next-menu-4').addClass('activated');
+    } else {
+        $('.next-menu-4').removeClass('activated');
     }
-    else{
-    	$('.next-menu-4').removeClass('activated');
+
+    if (stop > 4400) {
+        $('.next-menu-5').addClass('activated');
+    } else {
+        $('.next-menu-5').removeClass('activated');
     }
-    
-    if(stop>3800 && stop<=5100) {
-    	$('.next-menu-5').addClass('activated');
-    }
-    else{
-    	$('.next-menu-5').removeClass('activated');
-    }
-    
-    if(stop>5100) {
-    	$('.next-menu-6').addClass('activated');
-    }
-    else{
-    	$('.next-menu-6').removeClass('activated');
-    }
-    
-});
+
+}); 
+//@arkad Orginal Work
