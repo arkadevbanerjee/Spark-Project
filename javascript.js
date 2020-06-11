274ee5467f7f9fa63393386f881cd91ff450ee8f
Arkadev Banerjee <arkadev220701@gmail.com>
2020-06-11 10:13:36 +0530

    Last Update to repo

--- javascript.js
+++
@@ -1,8 +0,0 @@
- $(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if(scroll < 300){
-        $('nav').css('background', 'transparent');
-    } else{
-        $('nav').css('background', 'rgb(240, 173, 78)');
-    }
-});
